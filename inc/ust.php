08aede282279bc1da62de18da87c766ec0a9ab7b
Ahmet Çardak <79451091+Ahmet-cardak@users.noreply.github.com>
2023-04-09 19:07:22 +0300

    Update ust.php

--- inc/ust.php
+++ inc/ust.php
@@ -2,7 +2,7 @@
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Askan Bilgisayar</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
